c2288a8baa1915cc596d580f668efeaaba2723ca
Serge Bishyr <seriybg@gmail.com>
2017-01-25 10:13:24 +0200

    Add spring-sandbox project

---
+++ sbishyr-sandbox/src/test/java/sbishyr/sandbox/spring/SbishyrSandboxApplicationTests.java
@@ -0,0 +1,16 @@
+package sbishyr.sandbox.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbishyrSandboxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
